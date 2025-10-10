b4480fd5d70c48ea285cbc9610d6c4949d30322e
Dallas3 <dallasgordon3@gmail.com>
2025-10-10 16:28:38 -0700

    Project Portfolio setup

---
+++ react-portfolio-project/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+  return <div className="min-h-screen flex flex-col">
+    {/* Theme Toggles */}
+
+    {/* Starry Background Effect */}
+
+    {/* Navigation */}
+
+    {/* Main Content */}
+
+    {/* Footer */}
+  </div>
+}

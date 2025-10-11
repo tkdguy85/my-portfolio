19029a117f18377aeaf5f4808e28cc49e4af3a19
Dallas3 <dallasgordon3@gmail.com>
2025-10-10 17:25:27 -0700

    Set up Tailwind stuffs

--- react-portfolio-project/src/pages/Home.jsx
+++ react-portfolio-project/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 export const Home = () => {
-  return <div className="min-h-screen flex flex-col">
+  return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
     {/* Theme Toggles */}
 
     {/* Starry Background Effect */}

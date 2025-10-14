dd3a5d1531419fc5c5b30540b2eac1893535421a
Dallas3 <dallasgordon3@gmail.com>
2025-10-14 16:16:09 -0700

    Redesigned 404 page

--- react-portfolio-project/src/pages/NotFound.jsx
+++ react-portfolio-project/src/pages/NotFound.jsx
@@ -1,3 +1,8 @@
 export const NotFound = () => {
-  return <div>404 - Not Found</div>
+  return (
+    <section className="min-h-screen flex flex-col items-center justify-center bg-background text-foreground">
+      <div className="text-4xl pb-10">404 - Nothing to be found here...</div>
+      <a href="/" className="cosmic-button text-2xl text-white">Maybe we should go Home?</a>
+    </section>
+  ) 
 }

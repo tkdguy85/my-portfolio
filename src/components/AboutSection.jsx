4c5c342fe44a96a89ab7a6d0ecb2d468fb90ff68
Dallas3 <dallasgordon3@gmail.com>
2026-07-06 12:40:48 -0700

    Final tweaks

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -88,7 +88,7 @@ export const AboutSection = () => {
             <div className="flex flex-col sm:flex-row gap-4 pt-10 justify-center">
               <a href="#projects" className="cosmic-button">Check out my work</a>
               <a 
-                href="/my-portfolio/dallas-gordon-developer-resume.pdf" 
+                href="/my-portfolio/dallas_gordon_developer_resume.pdf" 
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/30 transition-colors duration-500"
               >Download My CV</a>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ export const HeroSection = () => {
       <div className="container max-w-4xl mx-auto text-center z-10">
         <div className="space-y-6">
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
-            <span className="opacity-0 animate-fade-in">Hello I'm </span>
+            <span className="opacity-0 animate-fade-in">Hello, I'm </span>
             <span className="text-primary opacity-0 animate-fade-in-delay-1">Dallas</span>
             <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">Gordon <span className="text-primary opacity-0 animate-fade-in-delay-1">III</span></span>
           </h1>
@@ -18,7 +18,7 @@ export const HeroSection = () => {
         
         <h2 className="text-2xl md:text-3xl font-bold my-10 text-glow">Welcome to My Portfolio</h2>
 
-        <p className="text-lg md:text-xl mb-8 text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">I'm a SoCal based full stack web developer that specializes in maintaining code. I love working in both a larger collaborative environment as well as just off to the side, in my own little playground. Peep my projects and learn more about me below. Thanks for stopping by!</p>
+        <p className="text-lg md:text-xl mb-8 text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">I'm a SoCal based software developer. I've previously worked for a branding agency in a fullstack role and am looking for my next fulltime adventure. I enjoy working in both a large, collaborative environments, as well as just off to the side, in my own little playground. Peep my projects and learn more about me below. Thanks for stopping by!</p>
 
         <div className="pt-4 opacity-0 animate-fade-in-delay-4">
           <a 
